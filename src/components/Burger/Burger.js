896c36b31e68b241519c68655733ca67a3a996ae
He Zhang <rbczhang90@gmail.com>
2019-03-05 11:35:32 -0800

    Another way to add dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,30 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        // Transform string value into array 
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key = {igKey + i} type={igKey} />;
-        });
+    // const transformedIngredients = Object.keys(props.ingredients)
+    // .map(igKey => {
+    //     // Transform string value into array 
+    //     return [...Array(props.ingredients[igKey])].map((_, i) => {
+    //         return <BurgerIngredient key = {igKey + i} type={igKey} />;
+    //     });
+    // });
+
+    let ingredientsArr = [];
+    // Flatten the 2-d ingredients array to 1d
+    Object.keys(props.ingredients).forEach(ingredient => {
+        for (let i = 0; i < props.ingredients[ingredient]; i++) {
+            ingredientsArr.push(ingredient);
+        }
     });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            {transformedIngredients}
+            {ingredientsArr.length === 0? 'please add some ingredients!' : 
+                ingredientsArr.map((ingredient, index)  => {
+                    return <BurgerIngredient key={index} type={ingredient}/>;
+                })
+            }
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );  
