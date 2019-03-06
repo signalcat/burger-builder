c261177f1cbe609452220b850937487e362c700a
He Zhang <rbczhang90@gmail.com>
2019-03-05 16:18:00 -0800

    Enable add ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
@@ -15,14 +23,29 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        // Make a copy of the ingredients object 
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        const priceAdd = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAdd;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render() {
         return (
           <Aux>
               <Burger ingredients={this.state.ingredients}></Burger>
-              <BuildControls/>
+              <BuildControls
+                  ingredientAdded={this.addIngredientHandler}
+              />
           </Aux>    
         );
     }
