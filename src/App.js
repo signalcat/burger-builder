08f47ef345543ac1bff52f746aecf5d3ac65ebc3
He Zhang <rbczhang90@gmail.com>
2019-03-26 18:42:16 -0700

    Add orders page and navigations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder}></Route>
           <Route path="/checkout" component={Checkout}></Route>
+          <Route path="/orders" component={Orders}></Route>
         </Layout>
       </div>
     );
