83c5320dfd48ebbeccb8925bf2d6ab763b2ff179
He Zhang <rbczhang90@gmail.com>
2019-04-05 16:22:49 -0700

    Adding dropdown selectors

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,16 +4,41 @@ import classes from './Input.module.css'
 const input = (props) => {
     // A warpper component for different input types
     let inputElement = null;
-    switch (props.inuttype) {
-        case ('input'):
-            // pass normal attributes through props
-            inputElement = <input className={classes.inputElement}{...props}/>
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
             break;
-        case ('textarea'):
-            inputElement = <textarea {...props}/>
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
             break;
         default:
-            inputElement = <input {...props}/>
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
     }
      
     return (
--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -7,11 +7,57 @@ import Input from '../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
@@ -50,12 +96,26 @@ class ContactData extends Component {
     }
 
     render () {
+
+        // Create an array to hold form elements configs to loop through 
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        console.log(formElementsArray);
+
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
-                <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Your street"/>
-                <Input inputtype="input" type="text" name="postal" placeholder="Your zipcode"/>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id} 
+                        elementType={formElement.config.elementType} 
+                        elementConfig={formElement.config.elementConfig} 
+                        value={formElement.config.value}></Input>
+                ))}
                 <Button btnType="Success" clicked={this.submitOrderHandler}>ORDER</Button>
             </form>
         );
