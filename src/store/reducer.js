6e42f0883d3dc043a5fe25df3142ea072092cdca
He Zhang <rbczhang90@gmail.com>
2019-04-29 17:49:14 -0700

    Add logics to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,39 @@
 import * as actionTypes from './actions';
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
-
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                // Don't forget to create a new nested object 
+                ingredients: {
+                    ...state.ingredients,
+                    // Use [] JS6 syntax to access a property
+                    // Overwrite the ingredients object copy
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
