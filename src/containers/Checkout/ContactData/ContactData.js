01570f1f71551250d7d3b22b75f5a26c48f6e91f
He Zhang <rbczhang90@gmail.com>
2019-07-10 11:59:54 -0700

    Add validation to login form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -142,15 +142,32 @@ class ContactData extends Component {
 
     checkValidity (value, rules) {
         let isValid = true;
-        if (rules.required && isValid) {
-            isValid = value.trim() !== '';
+        if (!rules) {
+            return true;
         }
-        if (rules.minLength && isValid) {
-            isValid = value.length >= rules.minLength;
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
-        if (rules.maxLength && isValid) {
-            isValid = value.length <= rules.maxLength;
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
         }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
 
