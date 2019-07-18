76e288c36719ad6d58a6b36670ae427b9fdbc78c
He Zhang <rbczhang90@gmail.com>
2019-07-18 10:00:27 -0700

    Fix minor display issues

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path="/" exact component={BurgerBuilder}></Route>
+          <Route path="/auth" component={Auth}></Route>
           <Route path="/logout" component={Logout}></Route>
           <Route path="/checkout" component={Checkout}></Route>
           <Route path="/orders" component={Orders}></Route>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ const sideDrawer = (props) => {
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={combinedClasses.join(' ')}>
+            <div className={combinedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo></Logo>
                 </div>
